61ecb2ce376a2c42ff7e5b90b23f439aef64c19e
Arash Bahrami <arash.bahrami@netseer.com>
2014-11-07 13:06:45 -0800

    fixing the hot patch TECHSUP-1366-arash

--- javaTest/src/com/netseer/javatest/helloWorld.java
+++ javaTest/src/com/netseer/javatest/helloWorld.java
@@ -11,8 +11,7 @@ public class helloWorld {
 		System.out.println("hot patch 2 added");
 		System.out.println("hot patch 3 is done, you can all relax now");
 		System.out.println("Fire has been put out in production for tech1357");
-		System.out.println("Fire has been put out in production for tech1357");
-		system.out.println("Patch for ticket TECHSUP-1366-arash-hot-fix-test is applied"); 
+		System.out.println("Patch for ticket TECHSUP-1366-arash-hot-fix-test is applied"); 
 	}
 
 }
